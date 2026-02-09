f66bb71db6d5d6fe5f6145d4f7ee91362371d3ea
tomosan078 <119410187+tomosan078@users.noreply.github.com>
2026-02-10 01:27:22 +0900

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,29 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Tomo Portfolio",
+  description: "Web Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>
+<body>
+  {/* 背景装飾 */}
+  <div className="bg-circle one" />
+  <div className="bg-circle two" />
+
+  {children}
+</body>
+{children}</body>
+    </html>
+  );
+}
